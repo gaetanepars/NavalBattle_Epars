5a3ee40f1d1c8bf80cd5de94ff55f51a0ecac6dc
Gaetan <gaetan.epars@cpnv.ch>
2020-12-10 15:59:40 +0100

    update pas terrible

--- main.c
+++ main.c
@@ -1,45 +1,80 @@
 #include <stdio.h>
 
-char grid [10][10];
+char printgrid [10][10];
+int  realgrid [10][10];
+
+char usercoordinateX;
+char usercoordinateY;
+
 int coordinateX;
 int coordinateY;
+
 int row;
 int column;
+
 int shipcount=0;
 
-int Fplay(){
-    char usercoordinateX;
-    char usercoordinateY;
+int Fmapgen(){            //print a map and generate another grid for the position of the ships
 
-    grid[0][0] = 'X'; //ship1
-    grid[3][1] = 'X'; //ship2
-    grid[7][2] = 'X'; //ship3
-    grid[8][8] = 'X'; //ship4
-    grid[7][9] = 'X'; //ship5
+    realgrid[0][0] = 2; //ship1
+    realgrid[3][1] = 2; //ship2
+    realgrid[7][2] = 2; //ship3
+    realgrid[8][8] = 2; //ship4
+    realgrid[7][9] = 2; //ship5
+
+    for (int row = 0; row < 10; ++row) {
+        for (int column = 0; column < 10; ++column) {
+            realgrid[row][column]= 0;
+        }
+    }
+
+    char Aj = 65;
+    printf("\n    1   2   3   4   5   6   7   8   9   10\n");
+    for (row = 0; row < 10; ++row) {
+        printf("%c", Aj);
+        Aj++;
+        for (column = 0; column < 10 ; ++column) {
+            printgrid[row][column]='~';
+            printf(" %3c", printgrid[row][column]);
+        }
+        printf("\n");
+    }
+}
+
+int Fplay(){                     //function that asks for coordinates and then check if there is a boat
 
     do {
         printf("\nChoose a coordinate: ");
 
-        scanf("%c", &usercoordinateX);
-        scanf("%c", &usercoordinateY);
+        scanf(" %c", &usercoordinateX);
+        scanf(" %c", &usercoordinateY);
 
-        coordinateX=usercoordinateX-65;
+        coordinateX=usercoordinateX-65;        //translates the user coordinates into real coordinates
         coordinateY=usercoordinateY-1;
 
-        if (grid[coordinateX][coordinateY] == 'X'){
-            printf("%3c", grid[row][column]);
-            printf("You have sunk a ship!");
+        if (realgrid[coordinateX][coordinateY] == 2){      //if there's a boat replace the wave with an X
+            printgrid[coordinateX][coordinateY]='X';
+            Fmapgen();
+            printf("\nYou've sunk a ship!\n");
             shipcount++;
-        } else if (grid[coordinateX][coordinateY] == '~'){
-            printf("%3c", grid[row][column]);
+
+        } else if (realgrid[coordinateX][coordinateY] == 1){  //if there is no boat replaces the water with a "*"
+            printgrid[coordinateX][coordinateY]='^';
+            Fmapgen();
+            printf("\nYou've hit a ship'!\n");
+
+        }else {
+            printgrid[coordinateX][coordinateY] = '*';
+            Fmapgen();
             printf("\nYou missed!\n");
         }
-    }while (shipcount!=5);
+    }while (shipcount!=5);                              //repeat the function until there are no ships left
 
 
 }
 
-int Fhelp(){
+int Fhelp(){                                //show tips
+
     printf("\nhelp1\n");
     printf("help2\n");
     printf("help3\n");
@@ -48,22 +83,9 @@ int Fhelp(){
     printf("Menu=>1 Exit=>0\n");
 }
 
-int Fmapgen(){
-    char Aj = 65;
-    printf("   1  2  3  4  5  6  7  8  9  10\n");
-    for (row = 0; row < 10; ++row) {
-        printf("%c", Aj);
-        Aj++;
-        for (column = 0; column < 10 ; ++column) {
-            grid[row][column] = '~';
-            printf("%3c", grid[row][column]);
 
-        }
-        printf("\n");
-    }
-}
 
-int main() {
+int main() {                                //menu
 
     int usermenuchoice;
     int endwhile;
@@ -96,7 +118,7 @@ int main() {
                 scanf("%d", &endwhile);
         }
 
-    } while (endwhile == 1);
+    } while (endwhile == 1);                    //Allow the user to go back to the menu
 
     return 0;
 }
